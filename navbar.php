742f823aea62d80d631209f3727bdd5118d7bbbb
HirushaR <hirusharandunu11@gmail.com>
2019-10-04 10:16:09 +0530

    third commit

--- navbar.php
+++ navbar.php
@@ -1,3 +1,25 @@
-<nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav mr-auto">
 
+        </ul>
+        <ul class="navbar-nav ">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown ml-auto">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><i class='far fa-user-circle' style='font-size:24px'></i></a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a href="#" class="dropdown-item">Reports</a>
+                    <a href="#" class="dropdown-item">Settings</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#"class="dropdown-item">Logout</a>
+                </div>
+            </li>
+        </ul>
+    </div>
 </nav>
