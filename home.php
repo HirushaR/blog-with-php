14bdef81b219a9ebe198085ab4e35e9198f450ca
HirushaR <hirusharandunu11@gmail.com>
2019-10-04 11:06:12 +0530

    fifth commit

--- home.php
+++ home.php
@@ -3,6 +3,8 @@
     <title>Home</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style4.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
 </head>
@@ -11,9 +13,17 @@
 include "navbar.php";
 ?>
 
+<div class="row">
+    <div class="col-sm-2">.col-sm-2</div>
+    <div class="col-sm-8">
+        <div class="container">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid aperiam atque aut explicabo iure, laboriosam libero magnam natus non porro sapiente sed totam. Ducimus eaque error in qui rem!
+        </div>
+    </div>
+    <div class="col-sm-2">.col-sm-2</div>
+</div>
+
 </body>
-
-
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
