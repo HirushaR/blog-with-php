17a846803ff1c4de166c44c218cf2d97eb775c19
HirushaR <hirusharandunu11@gmail.com>
2019-10-06 11:51:52 +0530

    show single post

--- function/functions.php
+++ function/functions.php
@@ -10,6 +10,7 @@
      if ($result) {
          while($row = mysqli_fetch_array($query))
          {
+            $id = $row['id'];
              $title = $row['title'];
              $content = $row['content'];
              $user = $row['user_id'];
@@ -21,22 +22,52 @@
              $email= $row['email'];
 
 
-
-
              echo "
                 <tr>
                         <td>$title</td>
                         <td>$email</td>
                         <td>$create</td>
                         <td>
-                            <button class=\"btn btn-secondary\">visit</button>
+                            <button class=\"btn btn-secondary\" id='visit-btn'>visit</button>
                             <button class=\"btn btn-primary\">Edit</button>
                         </td>
                 </tr>
-     
+                
+            <script type=\"text/javascript\">
+                document.getElementById(\"visit-btn\").onclick = function () {
+                    location.href = \"post.php?id=$id\";
+    };
+</script>
          ";
-         }
 
+         }
 
      }
  }
+
+
+
+
+function show_single_post()
+{
+    if (isset($_GET['id'])) {
+        global $con;
+        $id = $_GET['id'];
+        $show = "select * from posts where id= '".$id."'";
+        $query_1 = mysqli_query($con, $show);
+        $row = mysqli_fetch_assoc($query_1);
+        $title= $row['title'];
+        $content = $row['content'];
+        $user = $row['user_id'];
+        $create = $row['created_at'];
+
+        echo "
+                <b> <h3> $title</h3> </b> <br>
+                <i>$create</i><br>
+                <hr>
+                <p>$content </p>         
+        ";
+
+    }
+
+}
--- post.php
+++ post.php
@@ -4,7 +4,7 @@ include("function/functions.php")
 ?>
 <html>
 <head>
-    <title>Home</title>
+    <title>post</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -24,37 +24,15 @@ include "navbar.php";
     <div class="col-sm-1"></div>
     <div class="col-sm-8">
         <div class="container">
-            <div class="header">
-                <h3>POST LIST</h3>
-                <br>
-            </div>
-            <div class="table">
-                <table class="table">
-                    <tr>
-                        <th>Title</th>
-                        <th><i class='fas fa-user-tie'></i> Author</th>
-                        <th><i class="far fa-calendar-alt"></i> Publish At</th>
-                        <th><i class="fas fa-cogs"></i> Action</th>
-                    </tr>
-                    <?php
-                    get_post();
-                    ?>
-                </table>
-            </div>
+           <?php
+            show_single_post();
+           ?>
         </div>
+        <hr>
     </div>
-    <div class="col-sm-3" style="margin-top:50px;">
-        <button type="button" id="create-btn" class="btn btn-primary btn-lg"><i class="fa fa-plus"></i> Create a New post</button>
-    </div>
+
 </div>
 
 </body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script type="text/javascript">
-    document.getElementById("create-btn").onclick = function () {
-        location.href = "create_post.php";
-    };
-</script>
+
 </html>
